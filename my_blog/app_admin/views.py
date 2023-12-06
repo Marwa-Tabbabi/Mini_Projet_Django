bd446a2b584a80d994c094f3ecd7b260d7c32a2d
marwa <tababimarwa383@gmail.com>
2023-12-06 17:22:58 +0100

    first commit

---
+++ my_blog/app_admin/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import UpdateView, CreateView ,DeleteView
+from blog.models import Article
+from blog.forms import ArticleForm
+
+# Create your views here.
+def dashboard(request):
+    return render(request, 'db.html')
+
+def user_articles(request):
+    if not request.user.is_authenticated:
+        return redirect('home')
+
+    list_articles = Article.objects.filter(user=request.user)
+    return render(request, 'my-articles.html', {'list_articles': list_articles})
+
+class AddArticle(CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = "add-article.html"
+    success_url = "my-articles"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class UpdateArticle(UpdateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'app_admin/article_form.html'
+   
+
+class DeleteArticle(DeleteView):
+    model = Article
+    success_url = "/my-admin/my-articles"
+    
+
+
---
+++ my_blog/blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Category(models.Model):
+   name=models.CharField(max_length=120)
+
+   def __str__(self):
+        return self.name
+
+
+class Article (models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    title=models.CharField(max_length=50)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    desc=models.TextField()
+    image=models.ImageField(null=True,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("my-articles")
+       
+       
+   
+    
